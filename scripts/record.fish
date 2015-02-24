32a90db0665d5e66ff5c54bbe5d7848c3a7ff33d
Lucas Beyer <lucasb.eyer.be@gmail.com>
2015-02-24 18:39:31 +0100

    Make output of recording script a parameter.

--- scripts/record.fish
+++ scripts/record.fish
@@ -2,6 +2,11 @@
 
 set topics "/ptu/state"
 
+if not test -d $argv[1]
+  echo "Please pass an existing directory as first argument."
+  exit 1
+end
+
 if contains "head" $argv
   set topics "/head_xtion/rgb/camera_info" "/head_xtion/rgb/image_rect_color" $topics
   set topics "/head_xtion/depth/camera_info" "/head_xtion/depth/image_rect_meters" $topics
@@ -16,4 +21,4 @@ if contains "laser" $argv
   set topics "/scan" $topics
 end
 
-env ROS_MASTER_URI=http://karl:11311 bash -ic "rosbag record $topics -o /mnt/ffp/aaf/test"
+env ROS_MASTER_URI=http://karl:11311 bash -ic "rosbag record $topics -o $argv[1]"
